40d843c6e60b84e28122c71fdaaf4762ef0b3a22
camily-ghellar <ghellarcamily@gmail.com>
2025-06-26 23:15:41 -0300

    creating routes for trip, team and their relationship actions

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import express from 'express';
 import userRoutes from './routes/userRoutes.js';
 import studentRoutes from './routes/studentRoutes.js';
 import tripRoutes from './routes/tripRoutes.js';
+import teamRoutes from './routes/teamRoutes.js';
+import studentTeamRoutes from './routes/studentTeamRoutes.js';
+
 
 dotenv.config();
 
@@ -13,6 +16,9 @@ app.use(express.json());
 app.use('/user', userRoutes);
 app.use('/student', studentRoutes);
 app.use('/trip', tripRoutes);
+app.use('/team', teamRoutes);
+app.use('/studentTeam', studentTeamRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
